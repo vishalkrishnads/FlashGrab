340e4c2db379ffc7725bc350bc761d06012e7916
vishal-ds <321vishalds@gmail.com>
2020-11-25 19:05:23 +0530

    Added --save flag

--- manager.sh
+++ manager.sh
@@ -3,8 +3,8 @@
 clear
 printf "\t\t\tFlashGrab module installer\n\n"
 read -p "What do you want to install? >>> " module
-# printf "\nAnything other than the small letter 'y' is NO\n"
-# read -p "Do you want to save it?(y) >>> " save
+printf "\n\nAnything other than the small letter 'y' is NO\n"
+read -p "Do you want to save it?(y) >>> " save
 
 if [[ -d /backups ]]
 then
@@ -18,16 +18,14 @@ then
     sed -i.bak -e '25,26d' package.json
     rm package.json.bak
     printf "Installing your module\n"
-    npm install --save $module
-    # if [ $save == "y" ]
-    # then
-    #     npm install --save $module
-    # else
-    #     npm install $module
-    # fi
+    if [ $save == "y" ]
+    then
+        npm install --save $module
+    else
+        npm install $module
+    fi
     echo "Adding back modules to package.json"
     sed -i '25i\    "react-native-dark-mode": "^0.2.2",' package.json
-    sed -i '25i\    "react-native-fs": "^2.16.6",' package.json
     echo "Done."
     echo "Copying the modules back to node_modules/. This might take a while."
     cp -R backups/react-native-dark-mode node_modules/
