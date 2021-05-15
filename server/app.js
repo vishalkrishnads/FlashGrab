ba193996d0fab7a8f3c8ba63f8b0fa42c88f760f
Vishal Krishna <321vishalds@gmail.com>
2021-05-15 21:58:50 +0530

    Patched multi browser windows bug

--- server/app.js
+++ server/app.js
@@ -3,44 +3,62 @@ const express = require('express');
 const ws = require('ws');
 const { Builder } = require('selenium-webdriver');
 const app = express();
+const { Worker, isMainThread, parentPort, workerData } = require('worker_threads');
 const Purchaser = require('./purchaser')
 
-/*
-Here, we initialise a simple ws server with Express as middleware.
-So naturally, express will initiate a new webdriver session for each new incoming connection.
-But, there's a problem. One CPU thread can't handle multiple webdriver sessions at the same time.
-Hence, this creates a bug where more than one clients can't connect at the same time.
-Next commit hopes to solve it.
-*/
+  (async () => {
+    if (isMainThread) {
+      const wsServer = new ws.Server({ noServer: true });
+      wsServer.on('connection', socket => {
+        var worker
+        socket.on('message', async (message) => {
+          var data = JSON.parse(message)
+          if (!data.intermediate) {
+            worker = new Worker(__filename, { workerData: { data: data } })
+          } else {
+            worker.postMessage(data.intermediate)
+          }
+          worker.on("message", incoming => socket.send(incoming.message))
+          worker.on('exit', () => {
+            try { socket.close() } catch { }
+          })
+          socket.on('close', () => {
+            worker.postMessage('quit')
+          })
+        })
+      });
 
-const wsServer = new ws.Server({ noServer: true });
-wsServer.on('connection', socket => {
-  var driver
-  socket.on('message', async (message) => {
-    var data = JSON.parse(message)
-    if (!data.intermediate) {
-      driver = await new Builder().forBrowser('chrome').build()
-      purchaser = new Purchaser(driver, data, socket)
+      const server = app.listen(3000);
+      server.on('upgrade', (request, socket, head) => {
+        wsServer.handleUpgrade(request, socket, head, socket => {
+          wsServer.emit('connection', socket, request);
+        });
+      });
+    }
+
+    //Worker Thread
+    else {
+      var driver = await new Builder().forBrowser('chrome').build()
+      var purchaser = new Purchaser(driver, workerData.data, parentPort)
       // await purchaser.purchaser()
+      parentPort.on("message", async (message) => {
+        if (message === 'quit') {
+          try {
+            await driver.quit()
+            parentPort.close()
+          } catch { }
+        }
+        else { purchaser.intermediate(message) }
+      });
       try {
         await purchaser.purchaser()
       } catch (error) {
         if (error === "Abort") {
-          socket.send(`Aborting purchase`)
-          socket.close()
+          parentPort.postMessage({ "message": `Aborting purchase` })
+          await driver.quit()
+          parentPort.close()
         }
       }
-    } else {
-      await purchaser.intermediate(data.intermediate)
-      socket.close()
     }
-  })
-  socket.on('close', async() => { await driver.quit() })
-});
 
-const server = app.listen(3000);
-server.on('upgrade', (request, socket, head) => {
-  wsServer.handleUpgrade(request, socket, head, socket => {
-    wsServer.emit('connection', socket, request);
-  });
-});
+  })();
